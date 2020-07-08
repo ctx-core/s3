d2c8021a2c007b7060791c70c50efc03d54584b2
Brian Takita <brian.takita@gmail.com>
2020-07-08 01:13:49 -0700

    + index.ts

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,2 @@
 export declare const AWS_REGION: string;
+export declare function validate__env__s3(): void;
--- env.ts
+++ env.ts
@@ -1,6 +1,8 @@
 import { throw__missing__env } from '@ctx-core/env/env'
 export const AWS_REGION = process.env.AWS_REGION || 'us-east-1'
-if (!process.env.AWS_ACCESS_KEY_ID)
-	throw__missing__env('AWS_ACCESS_KEY_ID')
-if (!process.env.AWS_SECRET_ACCESS_KEY)
-	throw__missing__env('AWS_SECRET_ACCESS_KEY')
+export function validate__env__s3() {
+	if (!process.env.AWS_ACCESS_KEY_ID)
+		throw__missing__env('AWS_ACCESS_KEY_ID')
+	if (!process.env.AWS_SECRET_ACCESS_KEY)
+		throw__missing__env('AWS_SECRET_ACCESS_KEY')
+}
