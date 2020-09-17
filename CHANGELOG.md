739dfa47e834ba103458a1edea372d9dc4b9db3b
Brian Takita <brian.takita@gmail.com>
2020-09-17 15:30:10 -0700

    typescript updates

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# @ctx-core/s3
+
+## 6.0.3
+### Patch Changes
+
+- typescript updates
+- Updated dependencies [undefined]
+- Updated dependencies [undefined]
+  - @ctx-core/env@8.4.0
--- env.ts
+++ env.ts
@@ -1,8 +1,8 @@
-import { throw__missing__env } from '@ctx-core/env/env'
+import { throw_missing_env } from '@ctx-core/env'
 export const AWS_REGION = process.env.AWS_REGION || 'us-east-1'
 export function validate__env__s3() {
 	if (!process.env.AWS_ACCESS_KEY_ID)
-		throw__missing__env('AWS_ACCESS_KEY_ID')
+		throw_missing_env('AWS_ACCESS_KEY_ID')
 	if (!process.env.AWS_SECRET_ACCESS_KEY)
-		throw__missing__env('AWS_SECRET_ACCESS_KEY')
+		throw_missing_env('AWS_SECRET_ACCESS_KEY')
 }
