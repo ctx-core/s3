d4fb038477c2579e2f1842b3018565729a939b2c
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/last_modified_date_.js
+++
@@ -1,8 +0,0 @@
-/**
- * Returns the Date of LastModified
- */
-export function LastModified_date_({ LastModified }) {
-    return new Date(Date.parse(LastModified));
-}
-export { LastModified_date_ as _LastModified_date, LastModified_date_ as _date__LastModified__Object, };
-//# sourceMappingURL=src/last_modified_date_.js.map
--- src/s3_client_.js
+++
@@ -1,7 +0,0 @@
-import './env';
-import { S3Client } from '@aws-sdk/client-s3';
-export function S3Client_(config) {
-    return new S3Client(config);
-}
-export { S3Client_ as _S3Client, };
-//# sourceMappingURL=src/s3_client_.js.map
